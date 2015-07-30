bb71ac6210e46747bc9186ed9a4a6617022fde1b
Harald Pehl <harald.pehl@gmail.com>
2015-07-30 10:46:23 +0200

    Update samples/builder/builder

---
+++ samples/builder/target/generated-sources/gwt/com/google/gwt/lang/org_00046jboss_00046gwt_00046elemento_00046sample_00046builder_00046BuilderSample__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class org_00046jboss_00046gwt_00046elemento_00046sample_00046builder_00046BuilderSample__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
