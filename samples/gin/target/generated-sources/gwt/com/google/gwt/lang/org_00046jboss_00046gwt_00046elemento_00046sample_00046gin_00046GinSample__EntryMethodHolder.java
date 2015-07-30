b862b6e500d009d25a2f606962e98521965ea7d2
Harald Pehl <harald.pehl@gmail.com>
2015-07-30 10:32:50 +0200

    Cleanup after failed deploySample run

--- samples/gin/target/generated-sources/gwt/com/google/gwt/lang/org_00046jboss_00046gwt_00046elemento_00046sample_00046gin_00046GinSample__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class org_00046jboss_00046gwt_00046elemento_00046sample_00046gin_00046GinSample__EntryMethodHolder {
-  public static final void init() {
-    // to be synthesized later
-  }
-}
